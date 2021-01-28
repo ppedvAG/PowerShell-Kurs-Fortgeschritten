ad22dfe31508f384e9237e512d920fa02a6dcb74
Stefan Ober <StefanO@ppedv.de>
2021-01-28 14:55:36 +0100

    Parameter einfach gegen Falschbedienung gesichert

--- Skriptbeispiel/Get-LogonEvents.ps1
+++ Skriptbeispiel/Get-LogonEvents.ps1
@@ -1,8 +1,12 @@
 ﻿param(
-$Computername,
-$EventId ,
-$Newest 
+[string]$Computername = "localhost", #Parameter mit Defaultwert
+
+[Parameter(Mandatory=$true)] #Fügt dem Parameter erweiterte Eigenschaften hinzu, wie zb Mandatory um es zu einem Pflicht parameter zu machen
+[int]$EventId  ,
+
+[int]$Newest = 10 # Parameter mit Defaultwert
 )
+
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventID -eq $EventId | Select-Object -First $Newest
 
 
