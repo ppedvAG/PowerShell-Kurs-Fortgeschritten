201ee426757b40bd6eb5e99548b953a670ea0135
Stefan Ober <StefanO@ppedv.de>
2021-01-28 14:09:51 +0100

    Mögliche Lösung für WMI/CIM Übung hinzugefügt

---
+++ Lösungen/WMI-Uebung.ps1
@@ -0,0 +1,18 @@
+﻿#Aufgabe1
+Get-CimClass -Namespace root\cimv2 
+#oder
+Get-WmiObject -Namespace root\cimv2 -List
+
+#Aufgabe2
+Get-WmiObject -Class Win32_LogicalDisk
+#oder
+Get-CimInstance -ClassName Cim_PhysicalMemory
+
+#Aufgabe3
+Get-CimInstance -ClassName Win32_OperatingSystem | Format-List -Property *
+#oder
+Get-WmiObject -Class Win32_OperatingSystem | Format-List -Property *
+
+#Aufgabe4
+$session = New-CimSession -ComputerName Member -Credential ppedv\Administrator
+Get-CimInstance -CimSession $session -ClassName Win32_LogicalDisk
